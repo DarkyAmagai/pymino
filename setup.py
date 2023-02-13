fd4a6bd863dc91a256f6eaff5d1ae30b51f94007
cynical <94206142+forevercynical@users.noreply.github.com>
2023-02-12 17:29:45 -0800

    Update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name="pymino",
     license="MIT",
     author="forevercynical",
-    version="1.1.1.8",
+    version="1.1.1.9",
     author_email="me@cynical.gg",
     description="Amino API wrapper to make bots easier to use",
     url="https://github.com/forevercynical/pymino",
