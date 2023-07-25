fd3bf65560beef1a3c4c09cebb2e75aecd18b136
Mr. Pink <94206142+forevercynical@users.noreply.github.com>
2023-07-25 16:54:46 -0700

    Delete _global.py

--- pymino/ext/_global.py
+++
@@ -1,8 +0,0 @@
-from .utilities.generate import *
-
-class Global:
-    def __init__(self, session: HTTPClient):
-        """
-        WIP - TO BE FINISHED LATER
-        """
-        self.session = session
