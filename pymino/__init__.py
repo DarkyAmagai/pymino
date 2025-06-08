25f5f8872a8db3ee22aaca3906b26a0a7188ee84
Human <121132759+AngelicHuman@users.noreply.github.com>
2025-06-08 15:40:16 +0300

    Update __init__.py

--- pymino/__init__.py
+++ pymino/__init__.py
@@ -6,7 +6,7 @@ __title__ = 'pymino'
 __author__ = 'cynical'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2023 Cynical'
-__version__ = '1.3.3.5'
+__version__ = '1.3.3.6'
 __description__ = 'A Python wrapper for the aminoapps.com API'
 
 from .bot import Bot
