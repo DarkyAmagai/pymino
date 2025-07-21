e1e4e528004d4e8551a5981a4d258be39395406a
Human <121132759+AngelicHuman@users.noreply.github.com>
2025-07-21 20:58:56 +0300

    Update __init__.py

--- pymino/__init__.py
+++ pymino/__init__.py
@@ -1,43 +1,44 @@
 from typing import TYPE_CHECKING
 
-from pymino.bot import *
-from pymino.client import *
-__title__ = 'pymino'
-__author__ = 'cynical'
-__license__ = 'MIT'
-__copyright__ = 'Copyright 2023 Cynical'
-__version__ = '1.3.4.8'
-__description__ = 'A Python wrapper for the aminoapps.com API'
-
-__all__ = (
-    "Bot",
-    "Client",
-)
+from pymino.bot import Bot
+from pymino.client import Client
 
 __title__ = "pymino"
 __author__ = "cynical"
 __license__ = "MIT"
 __copyright__ = "Copyright 2023 Cynical"
-__version__ = "1.3.4.6"
+__version__ = "1.3.4.9"
 __description__ = "A Python wrapper for the aminoapps.com API"
 
-print("Join the pymino discord server: https://discord.gg/3HRdkVNets.")
+__all__ = ("Bot", "Client")
+
+print("Join the pymino Discord server: https://discord.gg/3HRdkVNets")
 
 if not TYPE_CHECKING:
     import requests
     import colorama
 
-    try:
-        latest_version = requests.get("https://pypi.org/pypi/pymino/json").json()[
-            "info"
-        ]["version"]
-    except Exception as e:
-        print(f"Failed to check the latest version: {e}")
+    colorama.init(autoreset=True)
+
+    get_latest_version = lambda: (
+        requests.get("https://pypi.org/pypi/pymino/json", timeout=5)
+        if requests.get("https://pypi.org/pypi/pymino/json", timeout=5).ok
+        else None
+    )
+
+    compare_versions = lambda current, latest: current != latest
+
+    response = get_latest_version()
+    latest = response.json().get("info", {}).get("version") if response else None
+
+    if latest:
+        if compare_versions(__version__, latest):
+            print("{}WARNING:{} You are using pymino version {} but the latest is {}. Run 'pip install --upgrade pymino'.".format(
+                colorama.Fore.YELLOW, colorama.Style.RESET_ALL, __version__, latest
+            ))
+        else:
+            print("{}pymino is up to date (version {}).{}".format(
+                colorama.Fore.GREEN, __version__, colorama.Style.RESET_ALL
+            ))
     else:
-        if __version__ != latest_version:
-            print(
-                f"{colorama.Fore.RED}WARNING:{colorama.Style.RESET_ALL} "
-                f"You are using an outdated version of pymino ({__version__}). "
-                f"The latest version is {latest_version}."
-            )
-        del latest_version
+        print("Could not fetch version info from PyPI.")
